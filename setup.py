1eb29cef074ac4ab013fd854cb3c141958020f82
Benjamin Mitchell <benjamin.neil.mitchell@gmail.com>
2021-01-13 01:10:53 -0500

    Added VPIP and PRF hand functions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="Pyker",
+    version="0.1dev",
+    packages=[
+        "poker",
+    ],
+    license="",
+    long_description=open("README.md").read(),
+)
