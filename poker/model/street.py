8bfdfa49c9a8ac9a639caa2135b22b5afd09015e
Benjamin Mitchell <benjamin.neil.mitchell@gmail.com>
2021-01-13 00:31:59 -0500

    Added street model

---
+++ poker/model/street.py
@@ -0,0 +1,22 @@
+"""This module defines a street or round of betting."""
+
+from dataclasses import dataclass
+from typing import List, Union
+
+from poker.model import actions
+
+
+@dataclass
+class Street:
+    """A Street is defined as a round of betting which consists of a collection of player actions."""
+
+    actions: List[Union[actions.ActionWithAmount, actions.Action]]
+
+    def __iter__(self):
+        return iter(self.actions)
+
+    def __repr__(self):
+        return f"Street(action={repr(self.actions)})"
+
+    def __str__(self):
+        return "\n".join([str(action) for action in self.actions])
