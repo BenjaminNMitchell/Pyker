e5000ac08b84b962bd1aa84ff519a3d319c39803
Benjamin Mitchell <benjamin.neil.mitchell@gmail.com>
2021-05-09 18:16:31 -0400

    Added liscence information and auther author contact info

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="pyker",
+    author="Benjamin Mitchell",
+    author_email="benjamin.neil.mitchell@gmail.com",
     version="0.0.1",
-    packages=setuptools.find_packages(),
-    license="",
-    long_description=open("README.md").read(),
+    packages=setuptools.find_packages(where="poker"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/BenjaminNMitchell/Pyker",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
